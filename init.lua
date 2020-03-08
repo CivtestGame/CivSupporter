5fafecc6f587ec5803a3c2673ad5c152e41cf4c6
R3R3R3 <>
2020-03-08 23:47:24 +0100

    Initial commit for CivSupporter

---
+++ init.lua
@@ -0,0 +1,10 @@
+
+local modpath = minetest.get_modpath(minetest.get_current_modname())
+
+civsupporter = {}
+
+dofile(modpath .. "/namecolor.lua")
+
+minetest.debug("[CivSupporter] Initialised.")
+
+return civsupporter
